7c3ae25d9d052b46818e3766184e819bd7f8989e
sneakingJ <jo.bl@gmx.de>
2022-02-06 17:50:10 +0100

    Nomination List: Divide long and short

--- app/Http/Livewire/NominationList.php
+++ app/Http/Livewire/NominationList.php
@@ -13,14 +13,20 @@ class NominationList extends Component
     /**
      * @var \Illuminate\Database\Eloquent\Collection
      */
-    public \Illuminate\Database\Eloquent\Collection $nominations;
+    public \Illuminate\Database\Eloquent\Collection $shortNominations;
+
+    /**
+     * @var \Illuminate\Database\Eloquent\Collection
+     */
+    public \Illuminate\Database\Eloquent\Collection $longNominations;
 
     /**
      * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
      */
     public function render(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $this->nominations = Nomination::orderBy('created_at', 'desc')->get();
+        $this->shortNominations = Nomination::where('short', 1)->orderBy('created_at', 'desc')->get();
+        $this->longNominations = Nomination::where('short', 0)->orderBy('created_at', 'desc')->get();
 
         return view('livewire.nomination-list');
     }
--- resources/views/livewire/nomination-list.blade.php
+++ resources/views/livewire/nomination-list.blade.php
@@ -5,25 +5,58 @@
         </div>
     </div>
 
-    <div class="box game-list" wire:poll>
-        @foreach($nominations as $nomination)
-            <div class="card mb-4">
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image igdb-cover">
-                                <img src="{{ $nomination->game_cover }}">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="title is-4">{{ $nomination->game_name }}</p>
-                            <p class="subtitle is-size-5">{{ \Illuminate\Support\Str::limit($nomination->pitch, 680, ' (...)') }}<br><br>
-                                <a href="{{ $nomination->game_url }}" target="_blank" class="is-size-6"><span class="icon is-small"><i class="fas fa-external-link-alt" aria-hidden="true"></i></span> <span>IGDB</span></a>
-                            </p>
+    <div class="box" wire:poll>
+        <div class="columns">
+            <div class="column is-half">
+                <h2 class="title is-3">Long Games</h2>
+            </div>
+            <div class="column is-half">
+                <h2 class="title is-3">Short Games</h2>
+            </div>
+        </div>
+        <div class="columns game-list">
+            <div class="column is-half">
+                @foreach($longNominations as $nomination)
+                    <div class="card mb-4">
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+                                    <figure class="image igdb-cover">
+                                        <img src="{{ $nomination->game_cover }}">
+                                    </figure>
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">{{ $nomination->game_name }}</p>
+                                    <p class="subtitle is-size-5">{{ \Illuminate\Support\Str::limit($nomination->pitch, 680, ' (...)') }}<br><br>
+                                        <a href="{{ $nomination->game_url }}" target="_blank" class="is-size-6"><span class="icon is-small"><i class="fas fa-external-link-alt" aria-hidden="true"></i></span> <span>IGDB</span></a>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
             </div>
-        @endforeach
+            <div class="column is-half">
+                @foreach($shortNominations as $nomination)
+                    <div class="card mb-4">
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+                                    <figure class="image igdb-cover">
+                                        <img src="{{ $nomination->game_cover }}">
+                                    </figure>
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">{{ $nomination->game_name }}</p>
+                                    <p class="subtitle is-size-5">{{ \Illuminate\Support\Str::limit($nomination->pitch, 680, ' (...)') }}<br><br>
+                                        <a href="{{ $nomination->game_url }}" target="_blank" class="is-size-6"><span class="icon is-small"><i class="fas fa-external-link-alt" aria-hidden="true"></i></span> <span>IGDB</span></a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 </div>
