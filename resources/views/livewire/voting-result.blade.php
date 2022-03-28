1e9e005319b82c060bea6848e824568b75824a4f
sneakingJ <jo.bl@gmx.de>
2022-03-28 19:27:46 +0200

    vote result diagram

--- resources/views/livewire/voting-result.blade.php
+++ resources/views/livewire/voting-result.blade.php
@@ -12,10 +12,10 @@
                     label: {
                         fontName: 'BlinkMacSystemFont,-apple-system,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",Helvetica,Arial,sans-serif',
                         fontSize: 16,
-                        color: '#ffffff',
-                        bold: true
+                        color: '#ffffff'
                     },
-                    nodePadding: 40,
+                    nodePadding: 60,
+                    labelPadding: 20,
                     enableInteractivity: false
                 }
             }
@@ -32,57 +32,51 @@
         });
     </script>
     <div class="box">
-        <div class="columns">
-            <div class="column">
-                <h2 class="title is-3">Long Games</h2>
-                <div id="long-sankey" class="sankey-container"></div>
-                @empty($longResult)
-                    No votes yet!
-                @endempty
-                <script type="text/javascript">
-                    function drawChartLong() {
-                        var data = new google.visualization.DataTable();
-                        data.addColumn('string', 'From');
-                        data.addColumn('string', 'To');
-                        data.addColumn('number', 'Votes');
-                        data.addRows([
-                            @foreach($longResult as $result)
-                                ['{!! addslashes($result[0]) !!}', '{!! addslashes($result[1]) !!}', {{ $result[2] }}],
-                            @endforeach
-                        ]);
+        <h2 class="title is-3">Long Games</h2>
+        <div id="long-sankey" class="sankey-container"></div>
+        @empty($longResult)
+            No votes yet!
+        @endempty
+        <script type="text/javascript">
+            function drawChartLong() {
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'From');
+                data.addColumn('string', 'To');
+                data.addColumn('number', 'Votes');
+                data.addRows([
+                    @foreach($longResult as $result)
+                        ['{!! addslashes($result[0]) !!}', '{!! addslashes($result[1]) !!}', {{ $result[2] }}],
+                    @endforeach
+                ]);
 
-                        // Instantiates and draws our chart, passing in some options.
-                        var chart = new google.visualization.Sankey(document.getElementById('long-sankey'));
-                        chart.draw(data, options);
-                    }
-                </script>
-            </div>
-            <div class="column is-narrow is-hidden-mobile">
-            </div>
-            <div class="column">
-                <h2 class="title is-3">Short Games</h2>
-                <div id="short-sankey" class="sankey-container"></div>
-                @empty($shortResult)
-                    No votes yet!
-                @endempty
-                <script type="text/javascript">
-                    function drawChartShort() {
-                        var data = new google.visualization.DataTable();
-                        data.addColumn('string', 'From');
-                        data.addColumn('string', 'To');
-                        data.addColumn('number', 'Votes');
-                        data.addRows([
-                            @foreach($shortResult as $result)
-                                ['{!! addslashes($result[0]) !!}', '{!! addslashes($result[1]) !!}', {{ $result[2] }}],
-                            @endforeach
-                        ]);
+                // Instantiates and draws our chart, passing in some options.
+                var chart = new google.visualization.Sankey(document.getElementById('long-sankey'));
+                chart.draw(data, options);
+            }
+        </script>
+    </div>
+    <div class="box">
+        <h2 class="title is-3">Short Games</h2>
+        <div id="short-sankey" class="sankey-container"></div>
+        @empty($shortResult)
+            No votes yet!
+        @endempty
+        <script type="text/javascript">
+            function drawChartShort() {
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'From');
+                data.addColumn('string', 'To');
+                data.addColumn('number', 'Votes');
+                data.addRows([
+                    @foreach($shortResult as $result)
+                        ['{!! addslashes($result[0]) !!}', '{!! addslashes($result[1]) !!}', {{ $result[2] }}],
+                    @endforeach
+                ]);
 
-                        // Instantiates and draws our chart, passing in some options.
-                        var chart = new google.visualization.Sankey(document.getElementById('short-sankey'));
-                        chart.draw(data, options);
-                    }
-                </script>
-            </div>
-        </div>
+                // Instantiates and draws our chart, passing in some options.
+                var chart = new google.visualization.Sankey(document.getElementById('short-sankey'));
+                chart.draw(data, options);
+            }
+        </script>
     </div>
 </div>
