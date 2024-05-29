5fc7e4d6f128bdf75de15337d67ef359d8f7e3b5
Ayaan Zaidi <zaidi@uplause.io>
2024-05-29 19:37:55 +0530

    feat(voting): divider card to separate ranked and unranked

---
+++ resources/views/livewire/cards/divider-card.blade.php
@@ -0,0 +1,9 @@
+<div class="card mb-4" style="border: 3px solid #ffcb1f;">
+    <div class="card-content">
+        <div class="content has-text-centered">
+            <i class="fa-solid fa-arrow-up"></i> Ranked <i class="fa-solid fa-arrow-up"></i>
+            <br>
+            <i class="fa-solid fa-arrow-down"></i> Unranked <i class="fa-solid fa-arrow-down"></i>
+        </div>
+    </div>
+</div>
